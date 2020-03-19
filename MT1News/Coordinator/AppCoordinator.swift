e413c69125de769fe74b76b573ce5c3b5d1958a3
ViniciusDeep <vinicius.correia@stone.com.br>
2020-03-19 15:11:29 -0300

    [Add] Coordinator with intial settings at project

---
+++ MT1News/Coordinator/AppCoordinator.swift
@@ -0,0 +1,30 @@
+//
+//  AppCoordinator.swift
+//  MT1News
+//
+//  Created by Vinicius Mangueira Correia on 19/03/20.
+//  Copyright © 2020 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import UIKit
+
+struct AppCoordinator: Coordinator {
+    let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        showListNews()
+    }
+    
+    func showListNews() {
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+    }
+    
+    
+}
+
+
---
+++ MT1News/Coordinator/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  MT1News
+//
+//  Created by Vinicius Mangueira Correia on 19/03/20.
+//  Copyright © 2020 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator {
+    func start()
+}
