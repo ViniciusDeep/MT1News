3c5a03b5316549ea08fe832b4f9efc1264c7c6f1
ViniciusDeep <vinicius.correia@stone.com.br>
2020-03-19 14:39:10 -0300

    Initial Commit

---
+++ MT1News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MT1News
+//
+//  Created by Vinicius Mangueira Correia on 19/03/20.
+//  Copyright © 2020 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MT1NewsTests/MT1NewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  MT1NewsTests.swift
+//  MT1NewsTests
+//
+//  Created by Vinicius Mangueira Correia on 19/03/20.
+//  Copyright © 2020 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import XCTest
+@testable import MT1News
+
+class MT1NewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
